57a9be8798a528b98c3fa522ea4315fb09e0392a
nongnapat2017 <nongnapat@gmail.com>
2019-08-22 13:33:16 +0700

    test download and delete

--- routes/web.php
+++ routes/web.php
@@ -35,8 +35,62 @@ Route::post('/upload', function() {
                 [
                     'name'     => 'file',
                     'contents' => fopen(request()->file('file'),'r+'),
+                ],
+                [
+                    'name'     => 'sub_path',
+                    'contents' => 'images',
                 ]
             ]
     ]);  
     return json_decode($response->getBody(), true);
+    $data = json_decode($response->getBody(), true);
+    $file = new App\File;
+    $file->file_id = $data['file']['id'];
+    $file->url = $data['url'];
+    $file->save();
+    return json_decode($response->getBody(), true);
+});
+
+Route::get('/download', function() {
+    $client = new GuzzleHttp\Client([
+        'base_uri' => env('STORATE_SERVICE_URL'),
+        'timeout'  => 8.0,
+    ]);
+    $response = $client->post('download', [
+            'headers' => [
+                'Accept' => 'application/json',
+                'token'  => env('STORAGE_SERVICE_TOKEN'), 
+                'secret' => env('STORAGE_SERVICE_SECRET')
+            ],
+            'multipart' => [
+                [
+                    'name'     => 'id',
+                    'contents' => 4,
+                ],
+            ]
+    ]);  
+    return json_decode($response->getBody(), true);
+
+});
+
+Route::get('/delete', function() {
+    $client = new GuzzleHttp\Client([
+        'base_uri' => env('STORATE_SERVICE_URL'),
+        'timeout'  => 8.0,
+    ]);
+    $response = $client->post('delete', [
+            'headers' => [
+                'Accept' => 'application/json',
+                'token'  => env('STORAGE_SERVICE_TOKEN'), 
+                'secret' => env('STORAGE_SERVICE_SECRET')
+            ],
+            'multipart' => [
+                [
+                    'name'     => 'id',
+                    'contents' => 8,
+                ],
+            ]
+    ]);  
+    return json_decode($response->getBody(), true);
+
 });
